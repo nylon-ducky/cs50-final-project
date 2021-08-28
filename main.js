e7097faa331b16c346cc9d239ac016aaaa4a851c
Durso <dursozd@gmail.com>
2021-08-28 16:19:59 -0400

    8-28

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
-// figure out how to structure the grid (table?)
 let cols, rows;
 let w = 75;
 let cells = [];
 let current;
 let rand;
 let stack = [];
+let solution = [];
+//let goal;
+let complete;
+let check = [];
 
-let starting = Date.now();
-console.log(starting);
+
+//let starting = Date.now();
+//console.log(starting);
 
 
-function difficulty() {
-  w = w / 2;
-  redraw()
-}
+
 // p5 required function
 function setup() {
   let cnv = createCanvas(750, 750);
   cnv.center('horizontal');
+
   cols = floor(width/w);
   rows = floor(height/w);
   // make the cells
@@ -29,47 +31,109 @@ function setup() {
   }
   // define current working cell
   current = cells[0];
+  cells[cells.length - 1].goal = true;
+  
+  solution.push(cells[0]);
 }
 // p5 required function
 function draw() {
+
+  //frameRate(5)
+
   background(79, 107, 81);
   for (let i = 0; i < cells.length; i++) {
     cells[i].display();
   }
+
+  //if(complete){
+    //rand = floor(random(0, stack.length + 1));
+    //current = stack[rand];
+  //}
+  
+  
+  if(current == undefined) {
+    for (let i = 0; i < cells.length; i++){
+      if(cells[i].visited == true){
+        check.push(cells[i]);
+      }
+    }
+    if (check.length == cells.length) {
+      //noLoop();
+      solve();
+
+      if(solution.length < 22) {
+        //clear();
+        window.location.reload()
+      }
+      
+      noLoop();
+      console.log(solution);
+      return;
+      
+    }else{
+      current = stack.pop();
+      check = [];
+    }
+  }
+  //console.log(current);
   current.visited = true;
   current.highlight(0, 255, 0);
   let next = current.checkNeighbors();
 
-  let ending = Date.now();
-  // check to see if maze is fully generated
-  if (stack.length == 0 && ending > starting + 100){
-
-    current.highlight(255,255,255)
-    console.log(current + current.walls);
-    noLoop();
-    console.log("stopped!");
-    solve(cells[0])
-  }
-
   if (next) {
+    if (!next.visited){
+      if (!complete){
+        solution.push(current);
+        if (next.goal) {
+          complete = true;
+          solution.push(next);
+        }
+      }
+    }
     next.visited = true;
     stack.push(current);
     removeWalls(current, next); 
     current = next;
+
   }
   // backtrack or random
-  else if (stack.length > 0 && stack.length < cells.length)
+  else if (!complete && solution.length > 0 && solution.length < cells.length)
   {
-    rand = floor(random(0, stack.length));
-    if (stack[rand].checkNeighbors()) {
-      current = stack[rand]
+    rand = floor(random(0, solution.length - 1));
+    if (solution[rand] != undefined && solution[rand].checkNeighbors() && !complete) {
+      current = solution[rand];
+      console.log(solution);
+      //if (!complete) {
+        /////////////////////////////////////////////
+        //if (solution[rand]['i'] > stack[stack.length - 1]['i']){
+          solution.splice(rand + 1, solution.length)
+          console.log(solution);
+        //}
+        
+        
+     // }
     }else{
-      current = stack.pop();
+      //current = stack.pop();
+      current = solution.pop();
+      
+      //if (!complete) {
+        //solution.pop();
+      //}
     }
+    /*
+    if (complete) {
+      for (let i = 0; i < solution.length; i++) {
+        solution[i].solutionCell = true;
+      }
+      
+    }*/
     
+  }else{
+    rand = floor(random(0, stack.length + 1));
+    current = stack[rand];
   }
 
-
+  solve();
 }
 
 
@@ -86,6 +150,8 @@ function Cell(i,j) {
   this.j = j;
   this.walls = [true,true,true,true];
   this.visited = false;
+  this.goal = false;
+  this.solutionCell = false;
   this.checkNeighbors = ()=> {
     let neighbors = [];
     
@@ -120,8 +186,17 @@ function Cell(i,j) {
     let y = this.j * w;
     noStroke();
     fill(r,g,b);
-    rect(x,y,50,50);
+    rect(x,y,w,w);
   }
+
+  this.thinlight = (r,g,b)=> {
+    let x = this.i * w + 25;
+    let y = this.j * w + 25;
+    noStroke();
+    fill(r,g,b);
+    rect(x ,y ,20,20);
+  }
+
   // display cell, control walls
   this.display = function() {
     let x = this.i * w;
@@ -174,6 +249,18 @@ function removeWalls(c,n) {
 
 }
 
+
+function solve() {
+  
+    for (let i = 0; i < solution.length; i++) {
+      solution[i].thinlight(255,255,255);
+    }
+  
+ 
+}
+
+
+/*
 let solstack = [];
 function solve(i) {
   
@@ -248,6 +335,8 @@ function solve(i) {
       solve(n);
     }
   }*/
+
+  /*
   console.log(neighbors.length)
   console.log(solstack.length)
   console.log(cells.length)
@@ -281,6 +370,7 @@ function solve(i) {
  
 }
 
+*/
 
 
 
@@ -292,25 +382,50 @@ function solve(i) {
 
 
 
-//*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
-
 // figure out how to structure the grid (table?)
-
 let cols, rows;
-let w = 37.5;
+let w = 75;
 let cells = [];
 let current;
+let rand;
 let stack = [];
+let solution = [];
+//let goal;
+let complete;
+let check = [];
+
+
+//let starting = Date.now();
+//console.log(starting);
+
 
 
 // p5 required function
 function setup() {
   let cnv = createCanvas(750, 750);
   cnv.center('horizontal');
-
-
 
   cols = floor(width/w);
   rows = floor(height/w);
@@ -323,36 +438,107 @@ function setup() {
   }
   // define current working cell
   current = cells[0];
-
+  cells[cells.length - 1].goal = true;
+  
+  solution.push(cells[0]);
 }
-
 // p5 required function
 function draw() {
+
+  //frameRate(5)
+
   background(79, 107, 81);
   for (let i = 0; i < cells.length; i++) {
     cells[i].display();
   }
-  current.visited = true;
-  current.highlight();
-
- 
-  let next = current.checkNeighbors();
-  
-  if (next) {
-    next.visited = true;
-
-    stack.push(current);
-
-    removeWalls(current, next); 
-
-    current = next;
-  }
-  // backtrack
-  else if (stack.length > 0){
+  if(current == undefined) {
+    for (let i = 0; i < cells.length; i++){
+      if(cells[i].visited == true){
+        check.push(cells[i]);
+      }
+    }
+    if (check.length == cells.length) {
+      //noLoop();
+      solve();
+      noLoop();
+      console.log(solution);
+      return;
+    }else{
       current = stack.pop();
+      check = [];
+    }
+  }
+  console.log(current);
+  current.visited = true;
+  current.highlight(0, 255, 0);
+  let next = current.checkNeighbors();
+
+  //let ending = Date.now();
+  // check to see if maze is fully generated
+  //if (stack.length == 0 ){//&& ending > starting + 100){
+
+    //urrent.highlight(255,255,255)
+    //console.log(current + current.walls);
+    //noLoop();
+    //console.log("stopped!");
+    //solve(cells[0])
+  //}
+
+
+  ///////////////////////////////////
+  if (next) {
+    if (!next.visited){
+      if (!complete){
+        solution.push(current);
+        if (next.goal) {
+          complete = true;
+          solution.push(next);
+        }
+      }
+    }
+    next.visited = true;
+    stack.push(current);
+    removeWalls(current, next); 
+    current = next;
+
+  }
+  // backtrack or random
+  else if (!complete && solution.length > 0 && solution.length < cells.length)
+  {
+    rand = floor(random(0, solution.length + 1));
+    if (solution[rand] != undefined && solution[rand].checkNeighbors() && !complete) {
+      current = solution[rand];
+      console.log(solution);
+      //if (!complete) {
+        /////////////////////////////////////////////
+        //if (solution[rand]['i'] > stack[stack.length - 1]['i']){
+          solution.splice(rand + 1, solution.length)
+          console.log(solution);
+        //}
+        
+        
+     // }
+    }else{
+      current = solution.pop();
+      
+      //if (!complete) {
+        //solution.pop();
+      //}
+    }
+    if (complete) {
+      for (let i = 0; i < solution.length; i++) {
+        solution[i].solutionCell = true;
+      }
+    }
+    
+  }else{
+    rand = floor(random(0, stack.length + 1));
+    current = stack[rand];
   }
 
+  solve();
 }
+
 
 // calculate index
 function index(i,j) {
@@ -361,16 +547,14 @@ function index(i,j) {
   }
   return i + j * cols;
 }
-
-
 //define a cell
 function Cell(i,j) {
   this.i = i;
   this.j = j;
   this.walls = [true,true,true,true];
   this.visited = false;
-
-
+  this.goal = false;
+  this.solutionCell = false;
   this.checkNeighbors = ()=> {
     let neighbors = [];
     
@@ -379,23 +563,18 @@ function Cell(i,j) {
     let right = cells[index(i+1,j)];
     let bottom = cells[index(i, j+1)];
     let left = cells[index(i-1,j)];
-
     if (top && !top.visited) {
       neighbors.push(top);
     }
-
     if (right && !right.visited) {
       neighbors.push(right);
     }
-
     if (bottom && !bottom.visited) {
       neighbors.push(bottom);
     }
-
     if (left &&!left.visited) {
       neighbors.push(left);
     }
-
     if (neighbors.length > 0) {
       let r = floor(random(0, neighbors.length));
       return neighbors[r];
@@ -403,24 +582,32 @@ function Cell(i,j) {
     else {
       return undefined;
     }
-
   }
-
-  this.highlight = ()=> {
+  // make the leading block bright green
+  this.highlight = (r,g,b)=> {
     let x = this.i * w;
     let y = this.j * w;
     noStroke();
-    fill(0, 255, 0);
+    fill(r,g,b);
     rect(x,y,w,w);
   }
 
+  this.thinlight = (r,g,b)=> {
+    let x = this.i * w + 25;
+    let y = this.j * w + 25;
+    noStroke();
+    fill(r,g,b);
+    rect(x ,y ,20,20);
+  }
 
   // display cell, control walls
   this.display = function() {
     let x = this.i * w;
     let y = this.j * w;
     stroke(255, 255, 0);
-    
+
+    strokeWeight(3);
+
     if (this.walls[0]) {
       line(x,y,x+w,y);
     }
@@ -434,7 +621,6 @@ function Cell(i,j) {
       line(x,y+w,x,y);
     }
     // if visited, make it obvious to me, the viewer
-
     if (this.visited) {
       noStroke();
       fill(0,0,0);
@@ -443,14 +629,9 @@ function Cell(i,j) {
     }
     
   }
-
 }
-
-
 function removeWalls(c,n) {
-
   let x = c.i - n.i;
-
   if (x == 1){
     c.walls[3] = false;
     n.walls[1] = false;
@@ -459,9 +640,7 @@ function removeWalls(c,n) {
     c.walls[1] = false;
     n.walls[3] = false;
   }
-
   let y = c.j - n.j;
-
   if (y == 1){
     c.walls[0] = false;
     n.walls[2] = false;
@@ -473,206 +652,288 @@ function removeWalls(c,n) {
 
 }
 
+
+function solve() {
+  
+    for (let i = 0; i < solution.length; i++) {
+      solution[i].thinlight(255,255,255);
+    }
+  
+ 
+}
+
+
 */
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 /*
 
-  let cols, rows;
-  let w = 25;
-  let cells = [];
-  let current;
-  let rand;
-  let stack = [];
-  
-  function difficulty() {
-    w = w / 2;
-    redraw()
+let cols, rows;
+let w = 75;
+let cells = [];
+let current;
+let rand;
+let stack = [];
+let solution = [];
+//let goal;
+let complete;
+let check = [];
 
+
+//let starting = Date.now();
+//console.log(starting);
+
+
+
+// p5 required function
+function setup() {
+  let cnv = createCanvas(750, 750);
+  cnv.center('horizontal');
+
+  cols = floor(width/w);
+  rows = floor(height/w);
+  // make the cells
+  for (let j = 0; j < rows; j++) {
+    for (let i = 0; i < cols; i++){
+      let cell = new Cell(i,j);
+      cells.push(cell);
+    }
+  }
+  // define current working cell
+  current = cells[0];
+  cells[cells.length - 1].goal = true;
+  
+  solution.push(cells[0]);
+}
+// p5 required function
+function draw() {
+
+  //frameRate(5)
+
+  background(79, 107, 81);
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].display();
   }
 
-  // p5 required function
-  function setup() {
-
-
-    let cnv = createCanvas(750, 750);
-    cnv.center('horizontal');
-
-
-
-    cols = floor(width/w);
-    rows = floor(height/w);
-    // make the cells
-    for (let j = 0; j < rows; j++) {
-      for (let i = 0; i < cols; i++){
-        let cell = new Cell(i,j);
-        cells.push(cell);
+  //if(complete){
+    //rand = floor(random(0, stack.length + 1));
+    //current = stack[rand];
+  //}
+  
+  
+  if(current == undefined) {
+    for (let i = 0; i < cells.length; i++){
+      if(cells[i].visited == true){
+        check.push(cells[i]);
       }
     }
+    if (check.length == cells.length) {
+      //noLoop();
+      solve();
 
-    // define current working cell
-    current = cells[0];
-
-  }
-
-  // p5 required function
-  function draw() {
-    background(79, 107, 81);
-    for (let i = 0; i < cells.length; i++) {
-      cells[i].display();
-    }
-
-    current.visited = true;
-    current.highlight(0, 255, 0);
-
-    let next = current.checkNeighbors();
-    if (stack.length == cells.length - 1){
-      current.highlight(0,0,0)
-      return;
-    }
-    if (next) {
-      next.visited = true;
-
-      stack.push(current);
-
-      removeWalls(current, next); 
-
-      current = next;
-    }
-    // backtrack
-    else if (stack.length > 0 && stack.length < cells.length)
-    {
-      rand = floor(random(0, stack.length));
-      if (stack[rand].checkNeighbors()) {
-        current = stack[rand]
-      }else{
-        current = stack.pop();
+      if(solution.length < 22) {
+        //clear();
+        window.location.reload()
       }
       
+      noLoop();
+      console.log(solution);
+      return;
+      
+    }else{
+      current = stack.pop();
+      check = [];
     }
-    console.log("done!")
   }
- 
+  //console.log(current);
+  current.visited = true;
+  current.highlight(0, 255, 0);
+  let next = current.checkNeighbors();
 
-  // calculate index
-  function index(i,j) {
-    if (i < 0 || j < 0 || i > cols-1 || j > rows-1) {
+  if (next) {
+    if (!next.visited){
+      if (!complete){
+        solution.push(current);
+        if (next.goal) {
+          complete = true;
+          solution.push(next);
+        }
+      }
+    }
+    next.visited = true;
+    stack.push(current);
+    removeWalls(current, next); 
+    current = next;
+
+  }
+  // backtrack or random
+  else if (!complete && solution.length > 0 && solution.length < cells.length)
+  {
+    rand = floor(random(0, solution.length - 1));
+    if (solution[rand] != undefined && solution[rand].checkNeighbors() && !complete) {
+      current = solution[rand];
+      console.log(solution);
+      //if (!complete) {
+        /////////////////////////////////////////////
+        //if (solution[rand]['i'] > stack[stack.length - 1]['i']){
+          solution.splice(rand + 1, solution.length)
+          console.log(solution);
+        //}
+        
+        
+     // }
+    }else{
+      //current = stack.pop();
+      current = solution.pop();
+      
+      //if (!complete) {
+        //solution.pop();
+      //}
+    }
+
+    
+  }else{
+    rand = floor(random(0, stack.length + 1));
+    current = stack[rand];
+  }
+
+  solve();
+}
+
+
+// calculate index
+function index(i,j) {
+  if (i < 0 || j < 0 || i > cols-1 || j > rows-1) {
+    return undefined;
+  }
+  return i + j * cols;
+}
+//define a cell
+function Cell(i,j) {
+  this.i = i;
+  this.j = j;
+  this.walls = [true,true,true,true];
+  this.visited = false;
+  this.goal = false;
+  this.solutionCell = false;
+  this.checkNeighbors = ()=> {
+    let neighbors = [];
+    
+    
+    let top = cells[index(i, j-1)];
+    let right = cells[index(i+1,j)];
+    let bottom = cells[index(i, j+1)];
+    let left = cells[index(i-1,j)];
+    if (top && !top.visited) {
+      neighbors.push(top);
+    }
+    if (right && !right.visited) {
+      neighbors.push(right);
+    }
+    if (bottom && !bottom.visited) {
+      neighbors.push(bottom);
+    }
+    if (left &&!left.visited) {
+      neighbors.push(left);
+    }
+    if (neighbors.length > 0) {
+      let r = floor(random(0, neighbors.length));
+      return neighbors[r];
+    }
+    else {
       return undefined;
     }
-    return i + j * cols;
+  }
+  // make the leading block bright green
+  this.highlight = (r,g,b)=> {
+    let x = this.i * w;
+    let y = this.j * w;
+    noStroke();
+    fill(r,g,b);
+    rect(x,y,w,w);
   }
 
+  this.thinlight = (r,g,b)=> {
+    let x = this.i * w + 25;
+    let y = this.j * w + 25;
+    noStroke();
+    fill(r,g,b);
+    rect(x ,y ,20,20);
+  }
 
-  //define a cell
-  function Cell(i,j) {
-    this.i = i;
-    this.j = j;
-    this.walls = [true,true,true,true];
-    this.visited = false;
+  // display cell, control walls
+  this.display = function() {
+    let x = this.i * w;
+    let y = this.j * w;
+    stroke(255, 255, 0);
 
+    strokeWeight(3);
 
-    this.checkNeighbors = ()=> {
-      let neighbors = [];
-      
-      
-      let top = cells[index(i, j-1)];
-      let right = cells[index(i+1,j)];
-      let bottom = cells[index(i, j+1)];
-      let left = cells[index(i-1,j)];
-
-      if (top && !top.visited) {
-        neighbors.push(top);
-      }
-
-      if (right && !right.visited) {
-        neighbors.push(right);
-      }
-
-      if (bottom && !bottom.visited) {
-        neighbors.push(bottom);
-      }
-
-      if (left &&!left.visited) {
-        neighbors.push(left);
-      }
-
-      if (neighbors.length > 0) {
-        let r = floor(random(0, neighbors.length));
-        return neighbors[r];
-      }
-      else {
-        return undefined;
-      }
-
+    if (this.walls[0]) {
+      line(x,y,x+w,y);
     }
-    // make th eleading block bright green
-    this.highlight = (r,g,b)=> {
-      let x = this.i * w;
-      let y = this.j * w;
+    if (this.walls[1]) {
+      line(x+w,y,x+w,y+w);
+    }
+    if (this.walls[2]) {
+      line(x+w,y+w,x,y+w);
+    }
+    if (this.walls[3]) {
+      line(x,y+w,x,y);
+    }
+    // if visited, make it obvious to me, the viewer
+    if (this.visited) {
       noStroke();
-      fill(r,g,b);
+      fill(0,0,0);
       rect(x,y,w,w);
-    }
-
-
-    // display cell, control walls
-    this.display = function() {
-      let x = this.i * w;
-      let y = this.j * w;
-      stroke(255, 255, 0);
-      
-      if (this.walls[0]) {
-        line(x,y,x+w,y);
-      }
-      if (this.walls[1]) {
-        line(x+w,y,x+w,y+w);
-      }
-      if (this.walls[2]) {
-        line(x+w,y+w,x,y+w);
-      }
-      if (this.walls[3]) {
-        line(x,y+w,x,y);
-      }
-      // if visited, make it obvious to me, the viewer
-
-      if (this.visited) {
-        noStroke();
-        fill(0,0,0);
-        rect(x,y,w,w);
-        
-      }
       
     }
-
+    
+  }
+}
+function removeWalls(c,n) {
+  let x = c.i - n.i;
+  if (x == 1){
+    c.walls[3] = false;
+    n.walls[1] = false;
+  }
+  else if (x == -1) {
+    c.walls[1] = false;
+    n.walls[3] = false;
+  }
+  let y = c.j - n.j;
+  if (y == 1){
+    c.walls[0] = false;
+    n.walls[2] = false;
+  }
+  else if (y == -1) {
+    c.walls[2] = false;
+    n.walls[0] = false;
   }
 
+}
 
-  function removeWalls(c,n) {
 
-    let x = c.i - n.i;
-
-    if (x == 1){
-      c.walls[3] = false;
-      n.walls[1] = false;
+function solve() {
+  
+    for (let i = 0; i < solution.length; i++) {
+      solution[i].thinlight(255,255,255);
     }
-    else if (x == -1) {
-      c.walls[1] = false;
-      n.walls[3] = false;
-    }
-
-    let y = c.j - n.j;
-
-    if (y == 1){
-      c.walls[0] = false;
-      n.walls[2] = false;
-    }
-    else if (y == -1) {
-      c.walls[2] = false;
-      n.walls[0] = false;
-    }
-
-  }
-
+  
+ 
+}
 
 */
